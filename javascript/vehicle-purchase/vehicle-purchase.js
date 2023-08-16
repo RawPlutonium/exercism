b210649555c4e6ec39fe603ef00117096065be95
RawPlutonium <lgithigi@gmail.com>
2023-08-16 15:55:53 +0300

    exercism prac

--- javascript/vehicle-purchase/vehicle-purchase.js
+++ javascript/vehicle-purchase/vehicle-purchase.js
@@ -11,7 +11,7 @@
  * @returns {boolean} whether a license is required
  */
 export function needsLicense(kind) {
-  throw new Error('Please implement the needsLicense function');
+  return kind === 'car' || kind === 'truck';
 }
 
 /**
@@ -23,7 +23,12 @@ export function needsLicense(kind) {
  * @returns {string} a sentence of advice which option to choose
  */
 export function chooseVehicle(option1, option2) {
-  throw new Error('Please implement the chooseVehicle function');
+  let tagLine = ' is clearly the better choice.';
+  if (option1 < option2) {
+    return option1 + tagLine;
+  } else {
+    return option2 + tagLine;
+  }
 }
 
 /**
@@ -35,5 +40,13 @@ export function chooseVehicle(option1, option2) {
  * @returns {number} expected resell price in the dealership
  */
 export function calculateResellPrice(originalPrice, age) {
-  throw new Error('Please implement the calculateResellPrice function');
+  if (age < 3) {
+    return originalPrice * 0.8;
+  } else {
+    if (age >= 3 && age <= 10) {
+      return originalPrice * 0.7;
+    } else {
+      return originalPrice * 0.5;
+    }
+  }
 }
